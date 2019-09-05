1c4ef0ae2847c27223f3a31aac3906e9c99e1cfa
Andrés Daníel Helgason <andresdanielhelgason@Andress-MacBook-Pro.local>
2019-09-05 10:13:52 +0000

    algorithm writtern for max_int

---
+++ max_int.py
@@ -0,0 +1,9 @@
+largest = 0
+num = 0 
+# Fill in the missing code
+while num > -1:
+    num = int(input("input a number: "))
+    if num > largest:
+        largest = num
+    
+print("The maximum is", largest)    # Do not change this line
